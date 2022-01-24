bc3b09ec015aeee08ebf4fc0b771ebd09074e597
Aurélie VIOLETTE <orwel.violette@gmail.com>
2022-01-24 20:09:00 +0100

    Update data

--- scripts/extract.js
+++ scripts/extract.js
@@ -1,56 +1,57 @@
-const rp = require("request-promise");
-const cheerio = require("cheerio");
-const parseDate = require("date-fns/parse");
-const formatDate = require("date-fns/format");
-const fs = require("fs");
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+const parseDate = require('date-fns/parse')
+const formatDate = require('date-fns/format')
+const fs = require('fs')
 
-const nbPage = 18;
+const nbPage = 18
+const PQ_TYPE = 18
 
-function getOptions(page) {
-    return {
-        uri: `https://fabtcg.com/events/?format=&type=18&query=&page=${page}&mode=event`,
-        useNewUrlParser: true,
-        transform: function (body) {
-            return cheerio.load(body);
-        },
-    };
+function getOptions(page, type = '') {
+  return {
+    uri: `https://fabtcg.com/events/?type=${type}&query=&page=${page}`,
+    useNewUrlParser: true,
+    transform: function (body) {
+      return cheerio.load(body)
+    },
+  }
 }
 
-async function scrapPage(page) {
-    let events = [];
-    try {
-        const $ = await rp(getOptions(page));
-        $(".event").each(function () {
-            const name = $(this).find("h2").text().replace("Pro Quest", "").trim();
-            const country = $(this).find("i.flag").attr("title");
-            const link = $(this).find("a").attr("href");
+async function scrapPage(page, type) {
+  let events = []
+  try {
+    const $ = await rp(getOptions(page, type))
+    $('.event').each(function () {
+      const name = $(this).find('h2').text().replace('Pro Quest', '').trim()
+      const country = $(this).find('i.flag').attr('title')
+      const link = $(this).find('a').attr('href')
 
-            const paragraphs = $(this).find("p");
-            const rawData = $(paragraphs[0]).text().split("\n")[1].trim().split(",")[0];
-            const date = parseDate(rawData, "EEE do MMM", new Date());
-            const day = parseInt(formatDate(date, "dd"));
-            const month = parseInt(formatDate(date, "MM"));
-            const address = $(paragraphs[1]).text().trim();
+      const paragraphs = $(this).find('p')
+      const rawData = $(paragraphs[0]).text().split('\n')[1].trim().split(',')[0]
+      const date = parseDate(rawData, 'EEE do MMM', new Date())
+      const day = parseInt(formatDate(date, 'dd'))
+      const month = parseInt(formatDate(date, 'MM'))
+      const address = $(paragraphs[1]).text().trim()
 
-            events.push({ name, country, link, date: { day, month }, address });
-        });
+      events.push({ name, country, link, date: { day, month }, address })
+    })
 
-        console.log(`${events.length} scrapped`);
-    } catch (err) {
-        console.log("Error", err);
-    }
-    return events;
+    console.log(`${events.length} scrapped`)
+  } catch (err) {
+    console.log('Error', err)
+  }
+  return events
 }
 
-async function scrap() {
-    let result = [];
-    for (let i = 1; i <= nbPage; i++) {
-        console.log(`Scrapping page ${i}`);
-        const events = await scrapPage(i);
-        result = result.concat(events);
-    }
-    console.log(`Finish ${result.length} tournaments extracted`);
-    fs.writeFileSync("tournaments.json", JSON.stringify(result, null, 4));
+async function scrap(type) {
+  let result = []
+  for (let i = 1; i <= nbPage; i++) {
+    console.log(`Scrapping page ${i}`)
+    const events = await scrapPage(i, type)
+    result = result.concat(events)
+  }
+  console.log(`Finish ${result.length} tournaments extracted`)
+  fs.writeFileSync('all-tournaments.json', JSON.stringify(result, null, 4))
 }
 
-scrap();
+scrap(PQ_TYPE)
